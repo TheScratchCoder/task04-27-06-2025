7c70ab30100c18b01e82cbdc5160c39736a500ef
Ankit Singh <143281507+TheScratchCoder@users.noreply.github.com>
2025-06-28 18:54:02 +0530

    Add files via upload

---
+++ task4-code-file.sql
@@ -0,0 +1,83 @@
+
+
+-- Table : Employees
+
+
+CREATE TABLE Employees (
+    EmployeeID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    JoiningDate DATE
+);
+
+
+-- Common aggregate functions:
+
+-- COUNT() – counts rows 
+-- SUM() – totals numeric values 
+-- AVG() – average
+-- MIN() / MAX() – lowest / highest 
+
+
+-- Examples ->
+
+
+-- Count all employees
+
+SELECT COUNT(*) AS TotalEmployees FROM Employees;
+
+
+-- Total salary paid
+
+SELECT SUM(Salary) AS TotalSalary FROM Employees;
+
+
+-- Average salary
+
+SELECT AVG(Salary) AS AverageSalary FROM Employees;
+
+
+-- Minimum and maximum salary
+
+SELECT MIN(Salary) AS MinSalary, MAX(Salary) AS MaxSalary FROM Employees;
+
+
+-- Use GROUP BY to Categorize Data
+-- Use GROUP BY to get summaries per category, e.g., department-wise:
+
+
+-- Count of employees in each department
+
+SELECT Department, COUNT(*) AS NumEmployees
+FROM Employees
+GROUP BY Department;
+
+
+-- Average salary in each department
+
+SELECT Department, AVG(Salary) AS AvgSalary
+FROM Employees
+GROUP BY Department;
+
+
+-- Filter Groups Using HAVING
+-- Use HAVING to filter groups (after GROUP BY) — unlike WHERE, which filters rows before grouping.
+
+
+-- Departments with more than 2 employees
+
+SELECT Department, COUNT(*) AS NumEmployees
+FROM Employees
+GROUP BY Department
+HAVING COUNT(*) > 2;
+
+
+-- Departments with average salary above 70,000
+
+SELECT Department, AVG(Salary) AS AvgSalary
+FROM Employees
+GROUP BY Department
+HAVING AVG(Salary) > 70000;
+
